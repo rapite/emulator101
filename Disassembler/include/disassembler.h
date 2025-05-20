9d86f060e6425f8899a3d2fa17437235660fd15e
rapite <andy02lee1@gmail.com>
2025-05-19 23:22:32 -0400

    setup todo for opcode function

--- Disassembler/include/disassembler.h
+++ Disassembler/include/disassembler.h
@@ -13,5 +13,16 @@
  */
 int init8080Op(const char* path, int pc);
 
+/**
+ * @brief Disassembles 8080Op
+ *
+ * Prints Opcode of program instruction based on case
+ *
+ * @param[in] path is file path to hexdump output
+ * @param[in] pc is program counter
+ *
+ * @return number of bytes of the op
+ */
+int Disassemble8080Op(unsigned char *codebuffer, int pc);
 
 #endif
