118d5659c87d60e2da198424f84d59833bc51b3a
rapite <andy02lee1@gmail.com>
2025-05-20 16:02:07 -0400

    fixed whitespace

--- Disassembler/src/disassembler.c
+++ Disassembler/src/disassembler.c
@@ -15,20 +15,19 @@ int init8080Op(const char* path, int pc) {
         exit(1);
     }
     //Get the file size and read it into a memory buffer    
-    fseek(fp, 0L, SEEK_END);    
-    int fsize = ftell(fp);    
-    fseek(fp, 0L, SEEK_SET);    
+    fseek(fp, 0L, SEEK_END);
+    int fsize = ftell(fp);
+    fseek(fp, 0L, SEEK_SET);
 
-    unsigned char *buffer=malloc(fsize);    
+    unsigned char *buffer=malloc(fsize);
 
-    fread(buffer, fsize, 1, fp);    
-    fclose(fp);    
+    fread(buffer, fsize, 1, fp);
+    fclose(fp);
 
-    int pc = 0;    
+    int pc = 0;
 
-    while (pc < fsize)    
-    {    
-        pc += Disassemble8080Op(buffer, pc);    
+    while (pc < fsize) {    
+        pc += Disassemble8080Op(buffer, pc);
     }  
 }
 
